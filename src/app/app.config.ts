987f8e5576bdb9d1554806e7836d01f86f27b6ca
luke.busch <luke.busch@gmail.com>
2026-02-06 16:27:36 +0000

    Added firebase dependencies to project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,18 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth())
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,49 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ChangeDetectionStrategy, Component, WritableSignal, inject, signal } from '@angular/core';
+import { Auth, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, signOut, User } from '@angular/fire/auth';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App {
-  protected readonly title = signal('myapp');
+  private auth: Auth = inject(Auth);
+  public user: WritableSignal<User | null> = signal(null);
+
+  constructor() {
+    this.auth.onAuthStateChanged((user) => {
+      this.user.set(user);
+    });
+  }
+
+  async login(email: string, pass: string): Promise<void> {
+    try {
+      const userCredential = await signInWithEmailAndPassword(this.auth, email, pass);
+      this.user.set(userCredential.user);
+    } catch (e) {
+      console.error(e);
+      alert('Invalid credentials!');
+    }
+  }
+
+  async loginWithGoogle(): Promise<void> {
+    try {
+      const provider = new GoogleAuthProvider();
+      const userCredential = await signInWithPopup(this.auth, provider);
+      this.user.set(userCredential.user);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  async logout(): Promise<void> {
+    try {
+      await signOut(this.auth);
+      this.user.set(null);
+    } catch (e) {
+      console.error(e);
+    }
+  }
 }
