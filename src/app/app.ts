eb36e2dabd7a70867060b68e8cb74b07b83635d8
Luke Busch <luke.busch@gmail.com>
2026-03-17 09:14:02 -0500

    updated dependencies, added display area to home page

--- src/app/app.ts
+++ src/app/app.ts
@@ -14,4 +14,10 @@ export class AppComponent {
   private database = inject(DatabaseService);
   // Strongly type the users$ observable
   users$ = this.database.getCollection<User>('users');
+
+  latestPairings = [
+    { reed: 'Vandoren Java Traditional 2.5', mouthpiece: 'Yamaha 4C', note: 'Great response and balanced tone', added: '2026-03-10' },
+    { reed: 'Rico Royal 2.5', mouthpiece: 'Theo Wanne Durga 5', note: 'Warm, rich low register', added: '2026-03-13' },
+    { reed: 'DAddario Reserve 2.5', mouthpiece: 'Selmer S80 C*', note: 'Smooth every dynamic', added: '2026-03-15' },
+  ];
 }
